3cd85421af35504adffc28c1c9c5b4843ec7a6ac
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-21 07:14:46 +0000

    Added database settings

--- testapp/testapp/settings.py
+++ testapp/testapp/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
