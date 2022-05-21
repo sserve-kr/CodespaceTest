e69e5e0f4975154207407964fa1cff09ac58b568
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-21 05:57:56 +0000

    Added index view

--- testapp/front/views.py
+++ testapp/front/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# 기존의 render import는 이 프로젝트에서 사용하지 않을 것이기 때문에 제거했다.
 
-# Create your views here.
+def index(request):
+  return HttpResponse('Hello World!')
