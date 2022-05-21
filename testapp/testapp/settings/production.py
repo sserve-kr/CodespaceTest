f4d53d51fee92309879bd72c88cbcd191318d490
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-21 08:06:05 +0000

    Add STATIC_ROOT

--- testapp/testapp/settings/production.py
+++ testapp/testapp/settings/production.py
@@ -7,3 +7,5 @@ ALLOWED_HOSTS = ['codespacetest.herokuapp.com', '127.0.0.1']
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
